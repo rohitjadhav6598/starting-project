3fef1be6878e71eea4c6bda3b75c5f88e0ed4668
Rohit Jadhav <rohitjadhav6598@gmail.com>
2023-03-05 14:40:53 +0530

    Fetch data from backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,24 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
   const [count, setCount] = useState(0);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -39,10 +52,15 @@ function PostsList({ isPosting, onStopPosting }) {
           <Post key={post.id} post={post} onRemovePost={removePostHandler} />
         ))}
       </ul>
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Fetching posts...</p>
         </div>
       )}
     </>
